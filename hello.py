ec32ab7248a53555ce69fb603d9d70a30f1e6768
poornima-metron <poornima@metronlabs.com>
2023-04-04 10:51:55 +0530

    check username and password

--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
-username = ''
-password = ''
-
-# This line should be allowed because it sets the 'word' variable to an empty string
-word = ""
 
 # These lines should be blocked by the pre-commit hook because they set the 'username' and 'password' variables to
 # non-empty strings
